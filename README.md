9d9d35065337a43deb80012423c6d1aa1c5444ca
unaG <veraguo11@163.com>
2024-03-20 15:29:23 +0800

    init and add triposr

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # unasora
 都爱搞大新闻，我也试试
+Not finished yet.
+
---
+++ main.py
@@ -0,0 +1,22 @@
+import argparse
+
+
+def train():
+    pass
+
+
+def inference():
+    pass
+
+
+def parse_args():
+    parser = argparse.ArgumentParser("Train or Inference UnaSora")
+    parser.add_argument("--")
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
